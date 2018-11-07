b4f3ba3e3244ace4c3909149c54c59bba93e910c
Adam Klein <adam@500tech.com>
2018-11-07 21:18:58 +0200

    Removed layout reference

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ import store from 'store';
 import theme from 'constants/themes.constants';
 
 import Localization from 'components/localization'; // TODO: remove if no localization
-import Layout from 'components/layout/layout';
 import Home from 'components/home';
 
 import './app.css';
